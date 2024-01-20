8bae4eeefac6870dcfdc47d992938be16b5c1433
som <somashekarayc@gmail.com>
2024-01-20 15:54:51 +0530

    22

--- app/libraries/Core.php
+++ app/libraries/Core.php
@@ -1,9 +1,5 @@
 <?php
-  /*
-   * App Core Class
-   * Creates URL & loads core controller
-   * URL FORMAT - /controller/method/params
-   */
+
   class Core {
     protected $currentController = 'Pages';
     protected $currentMethod = 'index';
--- app/models/User.php
+++ app/models/User.php
@@ -8,7 +8,6 @@
 
     public function register($data){
       $this->db->query('INSERT INTO users (name, email, password) VALUES(:name, :email, :password)');
-      // Bind values
       $this->db->bind(':name', $data['name']);
       $this->db->bind(':email', $data['email']);
       $this->db->bind(':password', $data['password']);
